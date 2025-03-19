45d8d6ab92c3177f8ab55adec8355278e228df27
SadeemRahman1 <Sadeemr935@gmail.com>
2025-03-19 22:28:25 +0500

    Fixed image path issue

--- src/Register/AdminSignIn.jsx
+++ src/Register/AdminSignIn.jsx
@@ -69,7 +69,7 @@ const AdminSignIn = () => {
       <div className="w-full max-w-md p-8 bg-white rounded-3xl shadow-xl divide-y divide-neutral-200">
         <div className="text-center pb-6">
          <img
-  src="https://github.com/SadeemRahman1/finalproject/blob/main/src/assets/NCBA%26E.png?raw=true"
+  // src="https://github.com/SadeemRahman1/finalproject/blob/main/src/assets/NCBA%26E.png?raw=true"
   alt="NCBA&E Logo"
   className="mx-auto h-40 w-40"
 />
